f157782a59c2f9b553ddd4be554c28e2a88fff8b
mcciaa24 <cian.mc_cann@student.reutlingen-university.de>
2024-12-20 02:30:49 +0000

    Added keywords + responses array. Added function to check if any words from the user's input appear in the keyResponses array and returns the response.

--- eliza/eliza.js
+++ eliza/eliza.js
@@ -1,17 +1,38 @@
+const keyResponses = [
+    { keys: ["CASSIUS", "LONGIUS"], response: "I knew it!" },
+    { keys: ["BRUTUS"], response: "Et tu, Brute?" },
+];
+
 function submitMessage() {
     var userMessage = document.getElementById("user-input").value;
     if (!userMessage) return;
 
     var conversation = document.getElementById("conversation");
-    conversation.innerHTML += userMessage
+    conversation.innerHTML += `<div class="user-message">${userMessage}</div>`;
 
-    var botResponse = getBotResponse(userMessage);
+    var botResponse = generateResponse(userMessage);
 
-    conversation.innerHTML += botResponse
+    conversation.innerHTML += `<div class="bot-response">${botResponse}</div>`;
 
     document.getElementById("user-input").value = "";
 }
 
-function getBotResponse(userMessage) {
-    return "OK";
+function generateResponse(userMessage) {
+    var defaultResponse = "Nevermind that, who got Caesar?";
+
+    for (var i = 0; i < keyResponses.length; i++) {
+        const selectedKeyResponse = keyResponses[i];    
+        if (containsAnyKeys(userMessage, keyResponses[i].keys)) {
+            return selectedKeyResponse.response;
+        }
+    }
+
+    return defaultResponse;
+}
+
+function containsAnyKeys(userMessage, keys) {
+    if (!keys || keys.length === 0) return false;
+
+    const message = userMessage.toUpperCase();
+    return keys.some(key => message.includes(key));
 }
